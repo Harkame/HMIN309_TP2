89d1850ca23bccc3fbfe57ef4978be46425f1b9c
achaillot <anthony-34400@hotmail.fr>
2018-10-02 12:27:23 +0200

    add search events and print all events

--- myApp/src/pages/events/events.ts
+++ myApp/src/pages/events/events.ts
@@ -1,36 +1,43 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
 
 @Component({
-  selector: 'page-list',
-  templateUrl: 'list.html'
+  selector: 'page-event',
+  templateUrl: 'events.html'
 })
-export class ListPage {
+export class EventPage {
   selectedItem: any;
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  items: Array<{title: string, type: string, description: string, date:string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
 
+    this.navCtrl.push(HomePage, {'events' : this.items});
     // Let's populate this page with some filler content for funzies
+    /*
     this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
     'american-football', 'boat', 'bluetooth', 'build'];
+    */
 
     this.items = [];
+
+    //Modifier les champs via accès au données de la bd. 
     for (let i = 1; i < 11; i++) {
       this.items.push({
         title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+        type: "Anniversary",
+        description: "test ma gueule",
+        date: "22/02/2222"
       });
     }
   }
 
   itemTapped(event, item) {
     // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
+    this.navCtrl.push(EventPage, {
       item: item
     });
   }
--- myApp/src/pages/home/home.ts
+++ myApp/src/pages/home/home.ts
@@ -1,14 +1,65 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('dateInput') dateInput;
+  types: Array<string>;
+  events: Array<any>;
+  allEvents: Array<any>;
+  selectedItem: String = "None";
+  selectedDate: String = "None";
+  checkedType:  String = "Date";
 
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.allEvents = this.navParams.get('events');
+  }
+
+  ngOnInit(): void {
+    this.setItems();
+  }
+
+  setItems(): void {
+    this.types = ['Anniversary', 'Work', 'Family', 'Fun'];
+  }
+
+  filterTypes(ev: any) {
+    this.setItems();
+    let val = ev.target.value;
+
+    if (val && val.trim() !== '') {
+      this.types = this.types.filter(function(item) {
+        return item.toLowerCase().includes(val.toLowerCase());
+      });
+    }
+  }
+
+  onClickOrSearchItem(choice: any){
+    this.selectedItem = choice;
+    console.log(this.selectedItem);
+  }
+
+  onSelectedDate(){
+    this.selectedDate = this.dateInput._text;
+    console.log(this.selectedDate);
+  }
+
+  onChecked(res : any){
+    this.checkedType = res;
+    console.log(this.checkedType);
+  }
+
+  setEventsArray(){
+    
+    if(this.checkedType == 'Date'){
+      
+    }
   }
 
 }
