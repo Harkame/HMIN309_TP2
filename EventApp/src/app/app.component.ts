78f65a99531da66320078274a8bab6f92d2c53da
Daviaud Louis <louis-richard.daviaud@etu.umontpellier.fr>
2018-12-29 03:22:03 +0100

    fix node_module

--- EventApp/src/app/app.component.ts
+++ EventApp/src/app/app.component.ts
@@ -28,24 +28,18 @@ export class MyApp {
   constructor(public platform: Platform,public databaseProvider: DatabaseProvider, public localNotifications: LocalNotifications,public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
-    // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
       { title: 'Events', component: EventPage },
       { title: 'Creation', component: CreationPage }
     ];
 
-    //this.setupNotification();
-
-    this.getEventOfDay();
   }
 
   initializeApp()
   {
     this.platform.ready().then(() =>
     {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
       this.databaseProvider.initialize();
@@ -56,14 +50,5 @@ export class MyApp {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
-  }
-
-  private getEventOfDay(){
-    //TODO FIX this line
-    /*
-    this.eventsOfTheDay = Observable.interval(36000000)
-                                    .mergeMapTo(this.eventService.getEventsOfTheDay)
-                                    .map(res => res.json());
-    */
   }
 }
--- EventApp/src/app/app.module.ts
+++ EventApp/src/app/app.module.ts
@@ -13,7 +13,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { LocalNotifications } from '@ionic-native/local-notifications';
 import { HttpModule } from '@angular/http';
-import {HttpClientModule} from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http'
 import { MyApp } from './app.component';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
